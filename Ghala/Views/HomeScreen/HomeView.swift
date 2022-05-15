82549f5abd729410788bcbf0147164bff2a80ead
joshua kilaha <jokilaha191@gmail.com>
2022-05-15 12:45:47 +0300

    init commit

---
+++ Ghala/Views/HomeScreen/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Ghala
+//
+//  Created by mroot on 15/05/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
