0668bc56e3d0b79008853a04ff777788368d3152
joshua kilaha <jokilaha191@gmail.com>
2022-05-19 18:52:56 +0300

    Views Created
    Views Created

---
+++ Ghala/Views/WareHouseView.swift
@@ -0,0 +1,20 @@
+//
+//  WareHouseView.swift
+//  Ghala
+//
+//  Created by mroot on 19/05/2022.
+//
+
+import SwiftUI
+
+struct WareHouseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WareHouseView_Previews: PreviewProvider {
+    static var previews: some View {
+        WareHouseView()
+    }
+}
