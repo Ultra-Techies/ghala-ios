960766be4f5131a2370a33e02305a5ff670d87b8
joshua kilaha <jokilaha191@gmail.com>
2022-05-27 22:52:03 +0300

    Fetch user details by user ID

--- Ghala/Views/SettingsView.swift
+++ Ghala/Views/SettingsView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @ObservedObject var userService = UserService()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        }.task {
+           await getUser()
+        }
+    }
+    func getUser() async {
+        do {
+            try await userService.getUser()
+        }catch {
+            print(error)
+        }
     }
 }
 
