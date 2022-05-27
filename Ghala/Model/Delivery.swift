bc4d24324e04f868fc934b4588712e3e20de7bca
joshua kilaha <jokilaha191@gmail.com>
2022-05-27 12:05:56 +0300

    Delivery Model

---
+++ Ghala/Model/Delivery.swift
@@ -0,0 +1,25 @@
+//
+//  Delivery.swift
+//  Ghala
+//
+//  Created by mroot on 27/05/2022.
+//
+
+import Foundation
+
+// MARK: - Delivery
+struct Delivery: Codable {
+    let id: Int
+    let route: String?
+    let orders: [Order] //from Order Model
+    let status: String
+    let deliveryWindow: String?
+    let warehouseID: Int
+    let noteCode, createdTime: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, route, orders, status, deliveryWindow
+        case warehouseID = "warehouseId"
+        case noteCode, createdTime
+    }
+}
