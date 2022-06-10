cb7b30257931c0e28bcb8f124528e56231d61cc0
joshua kilaha <jokilaha191@gmail.com>
2022-06-10 21:48:44 +0300

    Network view model

---
+++ Ghala/ViewModel/NetworkViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkViewModel.swift
+//  Ghala
+//
+//  Created by mroot on 10/06/2022.
+//
+
+import Foundation
+import Network
+
+class NetworkViewModel: ObservableObject {
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "NetworkViewModel")
+    @Published var isNotConnected = true
+    
+    
+    var imageName: String {
+        return isNotConnected ? "wifi" : "wifi.slash"
+    }
+    
+    var conncetionDescription: String {
+        return "No internet connection"
+    }
+    
+    init() {
+        monitor.pathUpdateHandler = { path in
+            DispatchQueue.main.async {
+                self.isNotConnected = path.status == .unsatisfied
+            }
+        }
+        monitor.start(queue: queue)
+    }
+    
+}
