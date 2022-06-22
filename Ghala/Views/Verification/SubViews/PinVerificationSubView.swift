66435eaf4c2bc6ffe6203ea27073e7eec2d59e2a
joshua kilaha <jokilaha191@gmail.com>
2022-06-22 15:06:23 +0300

    Pin Verification View Update

--- Ghala/Views/Verification/SubViews/PinVerificationSubView.swift
+++ Ghala/Views/Verification/SubViews/PinVerificationSubView.swift
@@ -7,15 +7,9 @@
 
 import SwiftUI
 struct PinVerificationSubView: View {
-    @ObservedObject var userService =  UserService()
     @ObservedObject var user: User
-    @State private var pinText: String = ""
-    @State private var pinFields: [String] = Array(repeating: "", count: 4)
+    @StateObject var  userViewModel = UserViewModel(userService: UserService())
     @FocusState var activeFiled: OTPField?
-    @State var loading: Bool = false
-    @State var showAlert: Bool = false
-    @State var errorMsg: String = ""
-    @State private var toHomeView = false
     var body: some View {
         VStack {
             OTPField()
@@ -23,7 +17,7 @@ struct PinVerificationSubView: View {
             // MARK: VERIFY Button
             Button {
                 Task {
-                    await checkPassword()
+                    await userViewModel.verifyPassword(user:user)
                 }
             } label: {
                 Text("Verify")
@@ -31,28 +25,28 @@ struct PinVerificationSubView: View {
                     .foregroundColor(.white)
             }
             .frame(maxWidth: .infinity, alignment: .center)
-            .background(Color.buttonColor).opacity(loading ? 0 :  1)
+            .background(Color.buttonColor).opacity(userViewModel.isLoading ? 0 :  1)
             .overlay {
                 ProgressView()
-                    .opacity(loading ? 1 : 0)
+                    .opacity(userViewModel.isLoading ? 1 : 0)
             }
             .disabled(checkStatus())
             .opacity(checkStatus() ? 0 : 1)
-            .fullScreenCover(isPresented: $toHomeView) {
-                ContentView(user: user)
+            
+            .fullScreenCover(isPresented: $userViewModel.toContentView) {
+                ContentView(user: userViewModel.user)
             }
         }
         .padding()
-        .onChange(of: pinFields) { newValue in
+        .onChange(of: userViewModel.pinTextFields) { newValue in
             OTPCondition(value: newValue)
         }
-        .alert(errorMsg, isPresented: $showAlert) {}
+        .alert(userViewModel.errorMsg, isPresented: $userViewModel.showAlert) {}
     }
-    
     // MARK: Check Button Status
     func checkStatus() -> Bool {
         for index in 0..<4 {
-            if pinFields[index].isEmpty {
+            if userViewModel.pinTextFields[index].isEmpty {
                 return true
             }
         }
@@ -63,7 +57,7 @@ struct PinVerificationSubView: View {
         //limit TextField
         for index in 0..<4 {
             if value[index].count > 1 {
-                pinFields[index] = String(value[index].last!)
+                userViewModel.pinTextFields[index] = String(value[index].last!)
             }
         }
         //move to Previous TextField
@@ -85,7 +79,7 @@ struct PinVerificationSubView: View {
         HStack(spacing: 14) {
             ForEach(0..<4, id: \.self) { index in
                 VStack {
-                    TextField("", text: $pinFields[index])
+                    TextField("", text: $userViewModel.pinTextFields[index])
                         .vCodeStyle()
                         .textContentType(.oneTimeCode)
                         .focused($activeFiled, equals: activeStateForIndex(index: index))
@@ -100,39 +94,6 @@ struct PinVerificationSubView: View {
         case 1: return .field2
         case 2: return .field3
         default: return .field4
-        }
-    }
-    func checkPassword() async {
-        do {
-            loading = true
-            pinText = pinFields.reduce("") { partialResult, value in
-                partialResult + value
-            }
-            print(pinText)
-            user.password = pinText
-            let response = try await userService.verifyUserLogin(user: user)
-            print("Status: \(response)")
-            
-            if response != 200 {
-                loading = false
-                let wrongPass = "Wrong Pin"
-                handleError(error: wrongPass)
-                print("Forbiden")
-            } else {
-                loading = false
-                try await userService.findByPhone(user: user)
-                toHomeView.toggle()
-            }
-        } catch {
-            handleError(error: error.localizedDescription)
-        }
-    }
-    
-    func handleError(error: String) {
-        DispatchQueue.main.async {
-            self.loading = false
-            self.errorMsg = error
-            self.showAlert.toggle()
         }
     }
 }
