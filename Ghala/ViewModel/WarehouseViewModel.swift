e369b04d06c279b35f7deb039135da554aa9f6c8
joshua kilaha <jokilaha191@gmail.com>
2022-06-24 14:12:20 +0300

    WareHouse controller initial

---
+++ Ghala/ViewModel/WarehouseViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  WarehouseViewModel.swift
+//  Ghala
+//
+//  Created by mroot on 24/06/2022.
+//
+
+import Foundation
+
+@MainActor
+class WarehouseViewModel: ObservableObject {
+    @Published var showAlert: Bool = false
+    @Published var errorMsg: String = ""
+    @Published var isLoading: Bool = false
+    @Published var searchWareHouse = ""
+    // Navigation
+    @Published var toAddWareHouse: Bool = false
+    
+    @Published var wareHouse = [WareHouse]()
+    
+    var wareHouseService: WareHouseService
+    init(wareHouseService: WareHouseService) {
+        self.wareHouseService = wareHouseService
+    }
+    
+    //  MARK: -Get All WareHouse
+    func getAll() async {
+        do {
+            let wareHouseData = try await wareHouseService.getAllWareHouse()
+            DispatchQueue.main.async {
+                self.wareHouse = wareHouseData
+            }
+
+        } catch {
+            handleError(error: error.localizedDescription)
+        }
+    }
+    // MARK: Search Warehouse
+    var wareHouseSearch: [WareHouse] {
+        if searchWareHouse.isEmpty {
+            return wareHouse
+        } else {
+            return wareHouse.filter { $0.name.localizedCaseInsensitiveContains(searchWareHouse)}
+        }
+    }
+    // MARK: Add Warehouse
+    func addWareHouse(warehouse: WareHouse) async {
+        do {
+            isLoading = true
+            print("Warehouse Name: \(warehouse.name)")
+            try await wareHouseService.addWareHouse(warehouse: warehouse)
+            isLoading = false
+        } catch {
+            handleError(error: error.localizedDescription)
+        }
+    }
+    // MARK: -Error
+    func handleError(error: String) {
+        DispatchQueue.main.async {
+            self.isLoading = false
+            self.errorMsg = error
+            self.showAlert.toggle()
+        }
+    }
+}
