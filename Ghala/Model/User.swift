a4745e35f96bf66bd197f7d13f6cfbb8e4bbf4d3
joshua kilaha <jokilaha191@gmail.com>
2022-06-26 23:30:00 +0300

    Update

--- Ghala/Model/User.swift
+++ Ghala/Model/User.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-/**
- 
-     {
-         "id": 4,
-         "email": "kk@gmail.com",
-         "phoneNumber": "+254748849086",
-         "assignedWarehouse": null,
-         "password": "$2a$10$JBAy9Zaqx9VksDRO51GKkuf1ANj.C8y2EbG.jM1nfhCsfZaFWSfre",
-         "role": "ADMIN",
-         "firstName": "Joshua",
-         "lastName": "Kilz",
-         "profilePhoto": null
-     }
- 
- */
-
-
 class User: Codable, ObservableObject, Identifiable {
     enum CodingKeys: CodingKey{
             case id
@@ -105,7 +88,6 @@ struct checkPhoneStatus: Codable {
 // MARK: TOKEN Response
 struct Token: Codable {
     let accessToken, refreshToken: String
-
     enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
         case refreshToken = "refresh_token"
@@ -115,78 +97,4 @@ struct Token: Codable {
 // MARK: OTP
 struct OTP: Codable {
     var otp: String
-}
-
-//class User: Codable, ObservableObject, Identifiable {
-//    enum CodingKeys: CodingKey{
-//            case id
-//            case phoneNumber
-//            case email
-//            case assignedWarehouse
-//            case firstName
-//            case lastName
-//            case password
-//        }
-//
-//        @Published var id: Int = 0
-//        @Published var phoneNumber = ""
-//        @Published var email = ""
-//        //@Published var assignedWarehouse: Int = 0
-//        @Published var firstName = ""
-//        @Published var lastName = ""
-//        @Published var password = ""
-//
-//    init() {
-//        }
-//
-//        required init(from decoder: Decoder) throws {
-//            let container = try decoder.container(keyedBy: CodingKeys.self)
-//
-//            id = try container.decode(Int.self, forKey: .id)
-//            phoneNumber = try container.decode(String.self, forKey: .phoneNumber)
-//            email = try container.decode(String.self, forKey: .email)
-//           // assignedWarehouse = try container.decode(Int.self, forKey: .assignedWarehouse)
-//            firstName = try container.decode(String.self, forKey: .firstName)
-//            lastName = try container.decode(String.self, forKey: .lastName)
-//            //password = try container.decode(String.self, forKey: .password)
-//        }
-//        func encode(to encoder: Encoder) throws {
-//            var container = encoder.container(keyedBy: CodingKeys.self)
-//            try container.encode(id, forKey: .id)
-//            try container.encode(phoneNumber, forKey: .phoneNumber)
-//            try container.encode(email, forKey: .email)
-//           // try container.encode(assignedWarehouse, forKey: .assignedWarehouse)
-//            try container.encode(firstName, forKey: .firstName)
-//            try container.encode(lastName, forKey: .lastName)
-//            try container.encode(password, forKey: .password)
-//        }
-//}
-
-// MARK: - User2
-class User2: Codable {
-    let id: Int
-    let email, phoneNumber: String
-    let assignedWarehouse: Int
-    let role, firstName, lastName: String
-    let profilePhoto: JSONNull?
-
-    init(id: Int, email: String, phoneNumber: String, assignedWarehouse: Int, role: String, firstName: String, lastName: String, profilePhoto: JSONNull?) {
-        self.id = id
-        self.email = email
-        self.phoneNumber = phoneNumber
-        self.assignedWarehouse = assignedWarehouse
-        self.role = role
-        self.firstName = firstName
-        self.lastName = lastName
-        self.profilePhoto = profilePhoto
-    }
-}
-
-// MARK: USER PIN Response
-struct Pin: Codable {
-    let verified: Bool
-}
-// MARK: Create User Response
-struct CreateUserResponse: Codable {
-    let id: Int
 }
