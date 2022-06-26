4ec086f096f32c01113d81f58315c2df425ab434
joshua kilaha <jokilaha191@gmail.com>
2022-06-26 20:52:39 +0300

    Updated to Codable & Hashable

--- Ghala/Model/Order.swift
+++ Ghala/Model/Order.swift
@@ -6,29 +6,76 @@
 //
 
 import Foundation
-// MARK: - Order
-struct Order: Codable {
-    let order: [Order]
-}
-// MARK: - OrderElement
-struct OrderElement: Codable, Equatable {
-    static func == (lhs: OrderElement, rhs: OrderElement) -> Bool {
+
+// MARK: Test For Encoding - Adding Order
+class Order: Codable, ObservableObject, Identifiable, Equatable, Hashable {
+    static func == (lhs: Order, rhs: Order) -> Bool {
         return lhs.id == rhs.id
     }
-    let id: Int
-    let createdDate, createdTime, due, deliveryWindow: String
-    let customerName, orderCode: String
-    let value: Int
-    let status: OrderStatus
-    let items: [Item]
-    let route: String
-    let warehouseID: Int
-
-    enum CodingKeys: String, CodingKey {
-        case id, createdDate, createdTime, due, deliveryWindow, customerName, orderCode, value, status, items, route
-        case warehouseID = "warehouseId"
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
-}
+
+     enum CodingKeys: CodingKey {
+         case id
+         case createdDate
+         case createdTime
+         case due
+         case deliveryWindow
+         case customerName
+         case orderCode
+         case value
+         case status
+         case items
+         case route
+         case warehouseId
+         
+     }
+
+     @Published var id: Int = 0
+     @Published var createdDate = ""
+     @Published var createdTime = ""
+     @Published var due = ""
+     @Published var deliveryWindow = ""
+     @Published var customerName = ""
+     @Published var orderCode = ""
+     @Published var value: Int = 0
+     @Published var status: OrderStatus = .pending
+     @Published var items: [Item] = .init()
+     @Published var route = ""
+     @Published var warehouseId: Int = 0
+
+     init() {
+     }
+     required init(from decoder: Decoder) throws {
+         let container = try decoder.container(keyedBy: CodingKeys.self)
+
+         id = try container.decode(Int.self, forKey: .id)
+         createdDate = try container.decode(String.self, forKey: .createdDate)
+         createdTime = try container.decode(String.self, forKey: .createdTime)
+         due = try container.decode(String.self, forKey: .due)
+         deliveryWindow = try container.decode(String.self, forKey: .deliveryWindow)
+         customerName = try container.decode(String.self, forKey: .customerName)
+         orderCode = try container.decode(String.self, forKey: .orderCode)
+         value = try container.decode(Int.self, forKey: .value)
+         status = try container.decode(OrderStatus.self, forKey: .status)
+         items = try container.decode([Item].self, forKey: .items)
+         route = try container.decode(String.self, forKey: .route)
+         warehouseId = try container.decode(Int.self, forKey: .warehouseId)
+     }
+         func encode(to encoder: Encoder) throws {
+             var container = encoder.container(keyedBy: CodingKeys.self)
+
+             try container.encode(customerName, forKey: .customerName)
+             try container.encode(due, forKey: .due)
+             try container.encode(deliveryWindow, forKey: .deliveryWindow)
+             try container.encode(items, forKey: .items)
+             try container.encode(route, forKey: .route)
+             try container.encode(warehouseId, forKey: .warehouseId)
+             try container.encode(deliveryWindow, forKey: .deliveryWindow)
+         }
+ }
 
 // MARK: - Item
 struct Item: Codable {
@@ -36,45 +83,9 @@ struct Item: Codable {
     let name: String
     let ppu, totalPrice: Int
 }
-// MARK: Status
+
 enum OrderStatus: String, Codable {
     case submitted = "SUBMITTED"
     case pending = "PENDING"
     case delivered = "DELIVERED"
-}
-
-struct DispachResponse: Codable {
-    let id: Int
-}
-//Mark: Order struct for Create delivery
-class OrderElementForDelivery: Codable, ObservableObject, Identifiable {
-    enum CodingKeys: CodingKey {
-        case orderIds
-        case route
-        case warehouseId
-        case deliveryWindow
-    }
-    @Published var orderIds: [Int] = [0]
-    @Published var route = "route1" //MARK: TO-DO fetch from view
-    @Published var warehouseId: Int = FromUserDefault.warehouseID
-    @Published var deliveryWindow = "MORNING" //MARK: TO-DO fetch from view
-    init() {
-    }
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        orderIds = try container.decode([Int].self, forKey: .orderIds)
-        route = try container.decode(String.self, forKey: .route)
-        warehouseId = try container.decode(Int.self, forKey: .warehouseId)
-        deliveryWindow = try container.decode(String.self, forKey: .deliveryWindow)
-
-    }
-        func encode(to encoder: Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
-
-            try container.encode(orderIds, forKey: .orderIds)
-            try container.encode(route, forKey: .route)
-            try container.encode(warehouseId, forKey: .warehouseId)
-            try container.encode(deliveryWindow, forKey: .deliveryWindow)
-        }
 }
